1037816c3bb8e1aba6b1379034a483ee540510ee
Martin Ellis <martinryanellis@gmail.com>
2023-07-18 22:32:10 +0100

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/edge";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  adapter: vercel(),
   integrations: [image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()],
-  output: "server"
+  }), react()]
 });
