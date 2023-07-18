19e88aac0e9e1f05b18ba337f27ca885dbbcc8f0
Martin Ellis <martinryanellis@gmail.com>
2023-07-18 22:22:44 +0100

    astro vercel install

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,14 @@ import image from "@astrojs/image";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/edge";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // ...
-  adapter: vercel({
-    analytics: true,
-  }),
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    react(),
-  ],
+  adapter: vercel(),
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), react()],
+  output: "server"
 });
