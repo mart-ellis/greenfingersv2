1d8ecca2944e8b7dd653ce86889e806f6d8b8035
Martin Ellis <martinryanellis@gmail.com>
2023-07-18 22:17:12 +0100

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()]
+  adapter: vercel({
+    analytics: true,
+  }),
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+  ],
 });
